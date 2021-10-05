d8b2d46a12b27eb0d694c0e309f3f7b9ac2382fb
MadhushaPrasad <madushaprasad21@gmail.com>
2021-10-05 23:55:59 +0530

    chore(route api):create crud methods

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res, next) => {
+//getAll products
+router.get('/products', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+//getAll single product
+router.get('/products/:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+//add single product
+router.post('/products/', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+//pudate single product
+router.patch('/products/:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+//delete single product
+router.delete('/products/:id', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
